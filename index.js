81d27f0a6d5aee0db1cffb5f105c95cf8c30be4a
cristian mejia <darkmejia643@gmail.com>
2020-11-11 02:16:38 +0000

    Primer intento

--- index.js
+++ index.js
@@ -4,36 +4,43 @@ let Encender= {
 }
 
 let CantidadGranos={
-  cantidad: true
+  cantidad: false
 }
 
-let Awa={
-  cantidad: true
+let Agua={
+  cantidad: false
 }
 
 
 let MolerGranos = {
   //Encender.call(this);
   moler: function(){
-    if (Encender.estado == false || CantidadGranos == false){
-      console.log("esta apagado, o no se puede moler");
+    if (Encender.estado == false){
+      console.log("esta apagado");
      // Encender.estado = true;
     }else{
       console.log("esta encendida");
+      
     }
   }
 
 }
 
 let CoffeMaker ={
-  start: function(){
-    
+
+  on: function(){
+    //Encender
+    Encender.estado = true;
+    console.log("La cafetera se ha encendido");
+  },
+
+  granos: function(){
+    CantidadGranos.estado = true
   }
 }
 
 let objCafe = Object.create(MolerGranos);//se crea el objeto
 objCafe.moler();
-objCafe.moler();
 
 let cofe= Object.create(CoffeMaker);
-cofe.start(Encender)
+cofe.on()
