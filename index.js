bce928d70b175e90d19eb56964ecd33bfd19accd
cristian mejia <darkmejia643@gmail.com>
2020-11-13 01:09:51 +0000

    Actualizado a una mejor implementacion del codigo

--- index.js
+++ index.js
@@ -1,46 +1,82 @@
-
-let Encender= {
-  estado : false
-}
-
-let CantidadGranos={
-  cantidad: false
-}
-
-let Agua={
-  cantidad: false
-}
-
-
-let MolerGranos = {
-  //Encender.call(this);
-  moler: function(){
-    if (Encender.estado == false){
-      console.log("esta apagado");
-     // Encender.estado = true;
-    }else{
-      console.log("esta encendida");
+'use strict'
+let TanqueDeAgua = {
+  //enciende:encendido,
+  agua: false,
+  calentarAgua: function(enciendo){
+    this.enciendo = enciendo;
+    if (this.agua === true && this.enciendo === true){
+      console.log("El agua se ha calentado correctamente");
+      return true;
       
+    }else if (this.agua==false && this.enciendo == true){
+      console.log("No Hay agua en el tanque, proceda a llenar");
+      return false;
+    }else{
+      console.log("No esta encendida la cafetera, por lo que no puedo proceder");
+      return false;
+    }
+  }
+};
+
+let Molino = {
+  granos: false,
+  on: function (enciendo) {
+   this.enciendo = enciendo;
+    if(this.granos === true && this.enciendo === true){
+      console.log("existen granos y esta encendido, procediendo a moler " );
+      return true;
+    }else if(this.granos === false && this.enciendo === true){
+      console.log("No hay granos"  );
+      return false;
+    }else{
+      console.log("No esta encendido");
+    }
+    
+  }
+};
+
+let Mesclador = {
+
+  prepararTaza: function(granos, agua){
+    this.granos = granos;
+    this.agua = agua;
+    if (this.granos === true && this.agua === true  ){
+      console.log("Procediendo a hacer taza de cafe");
+      console.log("Se ha preparado la taza de cafe");
+    }else if (this.granos == false && this.agua == true){
+      console.log("h");
     }
   }
 
 }
 
-let CoffeMaker ={
-
-  on: function(){
-    //Encender
-    Encender.estado = true;
-    console.log("La cafetera se ha encendido");
-  },
-
-  granos: function(){
-    CantidadGranos.estado = true
-  }
+let Hacercafe = {
+  enciendo: false,
+  molino: Molino,
+  tanqueDeAgua: TanqueDeAgua,
+  mesclador: Mesclador,
+  tazaDeCafe: function (){
+    let m = this.molino.on(this.enciendo);
+    let t; // this.t hace referencia a esta variable
+    
+    if(m){
+      this.t = this.tanqueDeAgua.calentarAgua(this.enciendo);
+    }
+    if (this.t){
+     // console.log(this.t)
+      //console.log(m)
+      this.mesclador.prepararTaza(m , this.t);
+    }
+    
+    
+   
+  } 
+  
 }
+  
 
-let objCafe = Object.create(MolerGranos);//se crea el objeto
-objCafe.moler();
 
-let cofe= Object.create(CoffeMaker);
-cofe.on()
+Hacercafe.enciendo = true;
+Hacercafe.molino.granos= true;
+Hacercafe.tanqueDeAgua.agua = true;
+Hacercafe.tazaDeCafe();
